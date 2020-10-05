a85a883758ed953e2aea2b2240931837fb5c5581
the-cutest-ogre-ever <coolest.ogre.on.the.swamp@gmail.com>
2020-10-05 23:18:56 +0300

    WebSocketConfig's problems were solved

--- src/main/java/ru/chat/swampy/config/WebSocketConfig.java
+++ src/main/java/ru/chat/swampy/config/WebSocketConfig.java
@@ -3,9 +3,11 @@ package ru.chat.swampy.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").withSockJS();
