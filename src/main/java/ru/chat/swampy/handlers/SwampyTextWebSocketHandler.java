edab8cf2b791d1cacfb5e26cedd86996c23fcdec
the-cutest-ogre-ever <coolest.ogre.on.the.swamp@gmail.com>
2020-10-06 23:52:44 +0300

    WebSocket handler was updated

--- src/main/java/ru/chat/swampy/handlers/SwampyTextWebSocketHandler.java
+++ src/main/java/ru/chat/swampy/handlers/SwampyTextWebSocketHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -18,16 +19,26 @@ public class SwampyTextWebSocketHandler extends TextWebSocketHandler {
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
         super.afterConnectionEstablished(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+        super.afterConnectionClosed(session, status);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         super.handleTextMessage(session, message);
+        sessions.forEach(webSocketSession -> {
+            try {
+                webSocketSession.sendMessage(message);
+            } catch (IOException e) {
+                LOGGER.error("Error occurred.", e);
+            }
+        });
     }
 
-    @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        super.afterConnectionClosed(session, status);
-    }
 }
