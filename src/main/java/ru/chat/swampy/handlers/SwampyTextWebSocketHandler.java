01921b83cb88f80e49e198556ad3ae5744db64c7
the-cutest-ogre-ever <coolest.ogre.on.the.swamp@gmail.com>
2020-10-06 23:47:35 +0300

    WebSocket handler was added

---
+++ src/main/java/ru/chat/swampy/handlers/SwampyTextWebSocketHandler.java
@@ -0,0 +1,33 @@
+package ru.chat.swampy.handlers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class SwampyTextWebSocketHandler extends TextWebSocketHandler {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(SwampyTextWebSocketHandler.class);
+
+    private final List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+    }
+}
