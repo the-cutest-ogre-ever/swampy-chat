ac106a68a3aeaf28cb45370208b5d0cd3d56d25c
the-cutest-ogre-ever <coolest.ogre.on.the.swamp@gmail.com>
2020-10-07 23:58:50 +0300

    some edits

--- src/main/java/ru/chat/swampy/config/WebSocketConfig.java
+++ src/main/java/ru/chat/swampy/config/WebSocketConfig.java
@@ -1,20 +1,18 @@
 package ru.chat.swampy.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.*;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import ru.chat.swampy.handlers.SwampyTextWebSocketHandler;
+
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+        webSocketHandlerRegistry.addHandler(new SwampyTextWebSocketHandler(), "/web-socket");
     }
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");
-    }
+
 }
