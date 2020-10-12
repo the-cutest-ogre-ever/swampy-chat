97e6ef87b7340a4f399a717cc07ed76609963d54
the-cutest-ogre-ever <coolest.ogre.on.the.swamp@gmail.com>
2020-10-12 23:55:56 +0300

    dto was added

---
+++ src/main/java/ru/chat/swampy/websocket/dto/ChatMessage.java
@@ -0,0 +1,23 @@
+package ru.chat.swampy.websocket.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+
+    private String from;
+    private String text;
+    private String recipient;
+    private String time;
+
+    public ChatMessage() {
+    }
+
+    public ChatMessage(String from, String text, String recipient) {
+        this.from = from;
+        this.text = text;
+        this.recipient = recipient;
+    }
+}
